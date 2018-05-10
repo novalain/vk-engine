cadc7b21ef8eb5a61d45f79f5963327f1f974a11
Michael Noven <michael.f.noven@gmail.com>
2018-05-10 21:25:01 +0200

    Set application info

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -12,8 +12,16 @@ Renderer::~Renderer() {
 }
 
 void Renderer::_initInstance() {
+	// Set up application info
+	VkApplicationInfo application_info {};
+	application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	application_info.apiVersion = VK_API_VERSION_1_1;
+	application_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+	application_info.pApplicationName = "Vulkan Engine";
+
 	VkInstanceCreateInfo instance_create_info{};
 	instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instance_create_info.pApplicationInfo = &application_info;
 
 	auto err = vkCreateInstance(&instance_create_info, nullptr, &mInstance);
 	if (err != VK_SUCCESS) {
